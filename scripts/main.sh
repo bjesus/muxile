259308b47b69d780fc4cf58b5759ef5e9ee0915d
Yo'av Moshe <bjesus@gmail.com>
2021-08-08 08:43:33 +0200

    file rename

--- scripts/main.sh
+++ scripts/main.sh
@@ -16,7 +16,7 @@ if test -S "/tmp/muxile.socket"; then
 else
     echo 'Starting up...'
     uuid=$(uuid)
-    setsid websocat --exec-sighup-on-stdin-close -tE unix-l:/tmp/muxile.socket reuse-raw:sh-c:'websocat -t - wss://muxile.zaraz.workers.dev/api/room/'"$uuid"'/websocket | xargs -n1 -d"\n" ./send-keys.sh' &>/dev/null & disown;
+    setsid websocat --exec-sighup-on-stdin-close -tE unix-l:/tmp/muxile.socket reuse-raw:sh-c:'websocat -t - wss://muxile.zaraz.workers.dev/api/room/'"$uuid"'/websocket | xargs -n1 -d"\n" ./keys-forwarder.sh' &>/dev/null & disown;
     echo $! > websocat.pid
     ./tmux_listener.sh &>/dev/null & disown;
     qrencode -m 2 -t utf8 <<< "https://muxile.zaraz.workers.dev/join/"$uuid
